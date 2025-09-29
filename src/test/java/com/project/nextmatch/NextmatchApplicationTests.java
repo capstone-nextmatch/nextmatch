c4fc75ad97a9f4cb98733a90f3237e1c35832d03
ndj <bluecode674@gmail.com>
2025-09-29 14:53:08 +0900

    first commit

---
+++ src/test/java/com/project/nextmatch/NextmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nextmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
