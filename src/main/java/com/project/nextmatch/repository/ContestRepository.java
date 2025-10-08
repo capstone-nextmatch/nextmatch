1f51d3e56a519ae53a49dc6a788ed9ab2e7d0dcf
ANMOOS <95952213+ANMOOS@users.noreply.github.com>
2025-10-08 16:19:07 +0900

    Update ContestRepository.java

--- src/main/java/com/project/nextmatch/repository/ContestRepository.java
+++ src/main/java/com/project/nextmatch/repository/ContestRepository.java
@@ -1,3 +1,7 @@
+/**
+ * Filename: ContestRepository.java
+ * Author: Sejun Park
+ */
 package com.project.nextmatch.repository;
 
 import com.project.nextmatch.domain.Contest;
@@ -6,4 +10,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // Contest 엔티티 (PK 타입은 Long)를 다루는 Repository
 public interface ContestRepository extends JpaRepository<Contest, Long> {
     // 필요한 경우, 나중에 커스텀 메서드를 추가할 수 있습니다.
+
 }
